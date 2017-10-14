4dd02a6a222ace6c6425e648ed3907e675ba3158
Mehtab kiran <mehtabkiran66@gmail.com>
2017-10-15 02:38:28 +0500

    Add project files.

---
+++ githubproj/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace githubproj.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
